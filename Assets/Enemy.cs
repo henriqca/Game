55ce2538c63621b5f77de24cddc165101f2e1957
henriqca <chenriquez_tag@hotmail.com>
2020-08-26 17:31:52 -0600

    Inicial
    Inicial

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public int health = 100;
+    public GameObject deathEffect;
+    public GameObject coin;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+        if ( health <= 0 ){
+            Die();
+        }
+    }
+
+    // Update is called once per frame
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Instantiate(coin, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
